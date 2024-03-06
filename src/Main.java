066628deca85808e52456823f2449f70deed6ee9
Praveen Chamod <praveen.c@fidenz.com>
2024-03-06 10:47:57 +0530

    Implement the logic for accepts a command line argument for the directory.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 1) {
+            System.out.println("Usage: java Main <directory-path>");
+            return;
+        }
+        String directoryPath = args[0];
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            System.out.println("Invalid directory path.");
+            return;
+        }
     }
 }
